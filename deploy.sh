c7864fe146b296b4b9dc26b86b29db8ccfad7e31
Shaunmak1214 <shaonmak@gmail.com>
2021-09-01 04:25:51 +0800

    deploy script (v0.1.17)

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+CURRENT_VERSION=$(node -p "require('./package.json').version")
 
 git add .
 
-git commit -m "$@"
+git commit -m "$@ (v$CURRENT_VERSION)"
 
 echo ===================================================
 echo Pushing to github branch $branch ...
